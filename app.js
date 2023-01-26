90ad7c639f89f365985b7b55e23cd50e137e5378
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2023-01-26 23:17:27 +0600

    make mongodb connection successfully

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose')
 const port = 8080;
 app.set('view engine', 'ejs')
 // setup static folder
@@ -11,6 +12,24 @@ app.use('/', homePageRouter);
 const projectRouter = require('./routes/projectRoute')
 app.use('/projects', projectRouter);
 
+const mongodbLink = 'mongodb+srv://Mohmmad_amir:O25AXHL31XRYud0G@cluster0.shp9rwm.mongodb.net/?retryWrites=true&w=majority'
+
+// database
+const database = module.exports = () => {
+    mongoose.set('strictQuery', true);
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+    try {
+        mongoose.connect(mongodbLink, connectionParams);
+        console.log('database connected successfully');
+    } catch (error) {
+        console.log(error);
+        console.log('database connect failed');
+    }
+}
+database()
 
 
 app.listen(port, () => {
