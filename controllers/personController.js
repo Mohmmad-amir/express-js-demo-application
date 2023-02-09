a7b20bc03d4b4b617993696d68b523d9aa91cca9
mohmmad Amir <test@mohmad.com>
2023-02-09 12:11:25 +0600

    complete show, index and delete function but delete methor not working

--- controllers/personController.js
+++ controllers/personController.js
@@ -2,6 +2,33 @@ const mongoose = require('mongoose')
 const Person = require('../models/personModel')
 
 /*
+*   index function
+*   this is for fetch all data from database
+ */
+exports.index = (req, res, next)=>{
+    Person.find()
+    .exec()
+    .then(docs =>{
+        // if (docs) {
+        //     console.log(docs);
+        //     res.status(200).json(docs)
+        // }else{
+        //     res.status(404).json({
+        //         message:"No valid entry found for provided ID"
+        //     })
+        // }
+            console.log(docs);
+            res.status(200).json(docs)
+
+    })
+    .catch(err=>{
+            console.log(err)
+            res.status(500).json({error:err})
+        })
+}
+
+
+/*
 *   store function
 *   this is for store data in database
  */
@@ -19,9 +46,56 @@ exports.store = (req, res) => {
     });
     person.save().then(result => {
         console.log(result);
+        res.status(201).json({
+            message: "Handling post request to /person ",
+            createdPerson: person
+        })
     }).catch(err => console.log(err));
-    res.status(201).json({
-        message: "Handling post request to /person ",
-        createdPerson: person
+
+}
+
+/*
+*   show function
+*   this is for show single data from database using ID
+ */
+exports.show = (req, res, next)=>{
+    const id = req.params.personID
+    Person.findById(id)
+    .exec()
+    .then(doc =>{
+        if (doc) {
+            console.log(doc);
+            res.status(200).json(doc)
+        }else{
+            res.status(404).json({
+                message:"No valid entry found for provided ID"
+            })
+        }
+
     })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({error:err})
+    })
+}
+
+/*
+*   delete function
+*   this is for delete single data from database using the ID
+ */
+
+exports.delete = (req, res, next)=>{
+    // const id = req.params.personID
+    // Person.remove( id )
+    // .exec()
+    // .then(result => {
+    //         res.status(200).json(result);
+    //     })
+    // .catch(err=>{
+    //     console.log(err)
+    //     res.status(500).json({error:err})
+    // })
+
+res.send("delete request")
+
 }
