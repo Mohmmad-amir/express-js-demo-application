90ed82ec4c7f37cb1956fd5037a92939350955c3
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2023-02-10 21:21:05 +0600

    install axios

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 const database = require('./db/database');
+const axios = require('axios');
 const app = express();
 const port = process.env.port || 8000;
 
@@ -24,7 +25,6 @@ app.use('/web', webRouter)
 //this route for return json data
 const apiRouter = require('./routes/api_route')
 app.use('/api', apiRouter)
-
 
 
 app.listen(port, () => {
